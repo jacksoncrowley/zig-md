9d6225a2070d8855e2369efed4a927f1fa5a101e
jackson <jacksonlcrowley@gmail.com>
2024-08-09 17:28:58 +0200

    restored old files - new version was dumb

--- src/particle.zig
+++
@@ -1,8 +0,0 @@
-const Vec3 = @import("vectors.zig").Vec3;
-
-pub const Particle = struct {
-    position: Vec3,
-    velocity: Vec3,
-    force: Vec3,
-    mass: f32,
-};
--- src/vectors.zig
+++
@@ -1,9 +0,0 @@
-pub const Vec3 = struct {
-    x: f32,
-    y: f32,
-    z: f32,
-
-    pub fn init(x: f32, y: f32, z: f32) Vec3 {
-        return Vec3{ .x = x, .y = y, .z = z };
-    }
-};
